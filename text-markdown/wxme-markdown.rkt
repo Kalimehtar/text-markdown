196f92d72f845f3c6ff7fc5e4e80573f6e92909f
Roman Klochkov <kalimehtar@mail.ru>
2021-11-21 19:32:42 +0500

    Added wxme/editor process
    Добавил обработку wxme/editor

--- text-markdown/wxme-markdown.rkt
+++ text-markdown/wxme-markdown.rkt
@@ -1,27 +1,30 @@
 #lang racket/base
 (provide wxme->markdown)
-(require racket/gui/base racket/class wxme racket/port net/base64)
+(require racket/gui/base racket/class wxme wxme/editor racket/port net/base64 )
 
 (define (wxme->markdown bytes)
   (define wport (wxme-port->port (open-input-bytes bytes)))
-  (call-with-output-string
-    (lambda (out)
-      (let loop ([d (read-char-or-special wport)])
-        (if (eof-object? d)
-            #f
-            (begin
-              (display
-                (cond
-                  [(char? d) d]
-                  [(is-a? d image-snip%)
-                   (bytes-append
-                    #"![](data:image/png;base64,"
-                    (base64-encode
-                     (let ([stream (open-output-bytes)])
-                       (send (send d get-bitmap) save-file stream 'png)
-                       (get-output-bytes stream))
-                     #"")
-                    #")")]
-                  [else (send d get-text 0 (send d get-count))])
-                out)
-              (loop (read-char-or-special wport))))))))
+  (define ((print-out wport) out)
+    (let loop ([d (read-char-or-special wport)])
+        (cond
+          [(eof-object? d) #f]
+          [(is-a? d editor%)
+           ((print-out (send d get-content-port)) out)
+           (loop (read-char-or-special wport))]
+          [else
+           (display
+            (cond
+              [(char? d) d]
+              [(is-a? d image-snip%)
+               (bytes-append
+                #"![](data:image/png;base64,"
+                (base64-encode
+                 (let ([stream (open-output-bytes)])
+                   (send (send d get-bitmap) save-file stream 'png)
+                   (get-output-bytes stream))
+                 #"")
+                #")")]
+              [else (send d get-text 0 (send d get-count))])
+            out)
+           (loop (read-char-or-special wport))])))
+  (call-with-output-string (print-out wport)))
